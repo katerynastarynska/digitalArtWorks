5ba3f739bb9efc8f354feceba4ed69828c0026f2
katerynastarynska <katerynastarynska@gmail.com>
2022-10-26 10:13:01 -0500

    login and signup added

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ app.get('/', (req, res) => {
     console.log("access route /, METHOD = GET")
     res.sendFile(path.join(__dirname, './client/index.html'));
 })
+app.get('/login', (req, res) => {
+    console.log("access route /, METHOD = GET")
+    res.sendFile(path.join(__dirname, './client/login.html'));
+})
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/signup.html'));
+})
+
 
 app.listen(port, () => {
     console.log("listening on port:", port)
 })
+module.exports = app;
