2156de999f7e8eed776c6d09667e0ec89cf0a622
katerynastarynska <katerynastarynska@gmail.com>
2022-10-27 10:24:27 -0500

    added page how-it-works

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, './client/public')));
 
 app.get('/', (req, res) => {
-    console.log("access route /, METHOD = GET")
     res.sendFile(path.join(__dirname, './client/index.html'));
 })
 app.get('/login', (req, res) => {
-    console.log("access route /, METHOD = GET")
+    // console.log("access route /, METHOD = GET")
     res.sendFile(path.join(__dirname, './client/login.html'));
 })
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, './client/signup.html'));
+})
+app.get('/how-it-works', (req, res) => {
+    console.log("access route /, METHOD = GET")
+    res.sendFile(path.join(__dirname, './client/how-it-works.html'));
 })
 
 app.listen(port, () => {
